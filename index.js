1a2c5a6e98e95846d9179d75a6a9b52acbbeeef6
Faisal Rasbihan <faisalrasbihan@users.noreply.github.com>
2018-04-25 16:11:13 +0700

    change port

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', function(req, res) {
 	res.send('Hello World');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 5000, function(){
 	console.log('Server started on port 3000...');
 });
